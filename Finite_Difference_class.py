e9ca3c8269dfc6002f68ab7ba62968a7ef4826f5
Piotr <piotr.granieczny26@gmail.com>
2025-09-28 22:41:42 +0200

    Main file update #1

--- Finite_Difference_class.py
+++ Finite_Difference_class.py
@@ -3,6 +3,7 @@ import pandas as pd
 import scipy
 import math
 import copy
+import matplotlib.pyplot as plt
 
 from PIL.ImageChops import offset
 from networkx.classes import non_edges
@@ -17,11 +18,6 @@ class Finite_Difference:
         # spatial steps
         self.M = M
 
-    # def create_grid(self, S_max) -> float | np.ndarray:
-    #     grid = np.array([np.array([np.array([S_max, i*self.delta_t]) for i in range(int(self.N / self.delta_t)+1)], dtype=float)] * int(self.M / self.delta_S))
-    #
-    #     return grid
-
     def explicit_method(self, rate, sigma, S_max, K, T):
 
         i = np.arange(1, self.M)
@@ -32,34 +28,29 @@ class Finite_Difference:
         b = 1 -dt * (i**2 * sigma**2 + rate)
         c = i * dt * (i * sigma**2 + rate) / 2
 
-        # call payoffs
+        # call payoffs (last column = present time)
         V = np.maximum(np.linspace(0, S_max, self.M+1) - K, 0)
 
-        V_new = V.copy()
+        V_new = copy.deepcopy(V)
+
+        # list for payoff values
+        payoffs_grid = []
 
         for _ in range(self.N):
             V_new[1:-1] = a * V[:-2] + b * V[1:-1] + c * V[2:]
+            payoffs_grid.append(V_new)
 
-        # tridiagonal matrix
-        # diagonals = [alpha[1:], beta, gamma[:-1]]
-        # offsets = [-1, 0, 1]
-        # A = scipy.sparse.diags(diagonals, offsets, shape=(self.M-1, self.M-1))
-        # A = A.tocsr()
-
-        # call payoffs
-        # V = np.maximum(np.linspace(0, S_max, self.M+1) - K, 0)
-
-        updated_grid = []
-
-        # backward evaluation
-        # for _ in range(self.N):
-        #     V[1:self.M] = A @ V[1:self.M]
-
+        return payoffs_grid
 
 
 # TESTS ################################################################################################################
 
 FD_test = Finite_Difference(0.1, 1, 1000, 100)
-print(FD_test.explicit_method(rate=0.05, sigma=0.02, S_max=200, K=100, T=1))
+Explicit_Method_run = FD_test.explicit_method(rate=0.05, sigma=0.02, S_max=200, K=100, T=1)
+print(len(Explicit_Method_run))
+
+
+
+
 
 
